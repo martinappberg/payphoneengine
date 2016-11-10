07bd822c7db3476c8e40c31fb010bff9e3edcb34
Martin <mkjberg@gmail.com>
2016-11-10 20:35:29 +0100

    new
    new

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/contacts", function(req, res) {
+});
+
+app.post("/contacts", function(req, res) {
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+});
+
+app.put("/contacts/:id", function(req, res) {
+});
+
+app.delete("/contacts/:id", function(req, res) {
+});
