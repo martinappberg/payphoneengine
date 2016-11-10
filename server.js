39c67268b3959a7b1de7a29aed2e3acfd4f3c06e
Martin <mkjberg@gmail.com>
2016-11-10 21:00:51 +0100

    Testing
    testing

--- server.js
+++ server.js
@@ -6,34 +6,73 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+var firebase = require("firebase");
+  var config = {
+    apiKey: "AIzaSyBp5xOVikCrlQBK0pxfU43Me2qy4zkMQh4",
+    authDomain: "payphone-f4a67.firebaseapp.com",
+    databaseURL: "https://payphone-f4a67.firebaseio.com",
+    storageBucket: "payphone-f4a67.appspot.com",
+    messagingSenderId: "202102323605"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
+//check if firebasedata exists
+
+
+/*  "/payments"
+ *    GET: finds all payments
+ *    POST: creates a new payment
  */
 
-app.get("/contacts", function(req, res) {
+app.get("/payments", function(req, res) {
 });
 
-app.post("/contacts", function(req, res) {
+app.post("/payments", function(req, res) {
+
+	var newPayment = req.body;
+
+  if (!(req.body.price || req.body.merchantID)) {
+    handleError(res, "Invalid user input", "Must provide a price and merchantID", 400);
+  }
+  usersRef.once('/merchants', function(snapshot) {
+  if (snapshot.hasChild(merchantID)) {
+  	//publish payment if merchant exists
+    function writeUserData(price, merchantID, uniqueID) {
+    	firebase.database().ref('payments/' + merchantID).set({
+    		unique: uniqueID,
+    		merchant: merchantID,
+    		amount : price
+    	});
+    }
+}else {
+  	handleError(res, "Invalid MerchantID","Please provide a valid one", 400);
+  }
+});
+  
+
 });
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
+
+/*  "/payments/:id"
+ *    GET: find payment by id
+ *    PUT: update payment by id
+ *    DELETE: deletes payment by id
  */
 
-app.get("/contacts/:id", function(req, res) {
+app.get("/payments/:id", function(req, res) {
 });
 
-app.put("/contacts/:id", function(req, res) {
+app.put("/payments/:id", function(req, res) {
 });
 
-app.delete("/contacts/:id", function(req, res) {
+app.delete("/payments/:id", function(req, res) {
 });
